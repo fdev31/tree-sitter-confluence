47e5da04ce2d16ae0f9404abd78172e5d3a5f5f6
fdev31 <fdev31@gmail.com>
2024-10-17 16:05:13 +0200

    add aerial query

---
+++ queries/aerial.scm
@@ -0,0 +1,35 @@
+(heading
+  (h1_heading (_) @name)
+  (#set! "kind" "Interface")
+  (#set! "level" "1")
+) @symbol
+
+(heading
+  (h2_heading (_) @name)
+  (#set! "kind" "Interface")
+  (#set! "level" "2")
+) @symbol
+
+(heading
+  (h3_heading (_) @name)
+  (#set! "kind" "Interface")
+  (#set! "level" "3")
+) @symbol
+
+(heading
+  (h4_heading (_) @name)
+  (#set! "kind" "Interface")
+  (#set! "level" "4")
+) @symbol
+
+(heading
+  (h5_heading (_) @name)
+  (#set! "kind" "Interface")
+  (#set! "level" "5")
+) @symbol
+
+(heading
+  (h6_heading (_) @name)
+  (#set! "kind" "Interface")
+  (#set! "level" "6")
+) @symbol
