8f86668d771303da50c675082e87d6c2dc317cb4
fdev31 <fdev31@gmail.com>
2026-04-14 23:18:36 +0200

    fix: update highlight queries to Neovim 0.12 capture names and add generated parser
    - Replace deprecated capture names (@text.emphasis, @text.strong,
      @parameter, @field, @symbol, @annotation, etc.) with modern
      @markup.* / @variable.* equivalents
    - Use semantic @markup.heading.N levels for h1-h6
    - Include generated src/parser.c and src/node-types.json so users
      can compile without tree-sitter-cli generate step

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,22 +1,22 @@
-;(strikethrough) @comment
-(italic) @text.emphasis @spell
-(bold) @text.strong @spell
-(color) @parameter
+;(strikethrough) @markup.strikethrough
+(italic) @markup.italic @spell
+(bold) @markup.strong @spell
+(color) @variable.parameter
 (colorcode) @string
-(h1_heading) @keyword @spell
-(h2_heading) @function @spell
-(h3_heading) @comment.warning @spell
-(h4_heading) @label @spell
-(h5_heading) @constructor @spell
-(h6_heading) @markup.heading @spell
-(table_header) @variable
+(h1_heading) @markup.heading.1 @spell
+(h2_heading) @markup.heading.2 @spell
+(h3_heading) @markup.heading.3 @spell
+(h4_heading) @markup.heading.4 @spell
+(h5_heading) @markup.heading.5 @spell
+(h6_heading) @markup.heading.6 @spell
+(table_header) @markup.heading
 (table_header_cell) @attribute
-(table_cell) @field
-(monospace) @symbol
-(code_block) @symbol
-(quote_block) @annotation
-(panel_block) @annotation
+(table_cell) @variable.member
+(monospace) @markup.raw
+(code_block) @markup.raw.block
+(quote_block) @markup.quote
+(panel_block) @markup.quote
 (key_value) @attribute
-(link) @markup.underline
-(image) @constructor
+(link) @markup.link
+(image) @markup.link
 (text) @spell
